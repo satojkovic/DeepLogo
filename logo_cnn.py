895d690e9dd056dd4e13d5913cdee4ba202648f0
satojkovic <satojkovic@gmail.com>
2016-06-22 08:08:31 +0900

    Define flags

--- logo_cnn.py
+++ logo_cnn.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 
+FLAGS = tf.app.flags.FLAGS
+
+tf.app.flags.DEFINE_string(
+    "train_dir", "logo_images/cropped",
+    "Directory where to write event logs and checkpoint.")
+
+
+def train():
+    pass
+
 
 def main():
-    pass
+    if not tf.gfile.Exists(FLAGS.train_dir):
+        print("Not found: %s" % (FLAGS.train_dir))
+    train()
 
 
 if __name__ == '__main__':
