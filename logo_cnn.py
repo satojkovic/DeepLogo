f6ea0dccb3da5b68b67a5fbe8af2fe41c21d9f8f
satojkovic <satojkovic@gmail.com>
2016-06-25 13:22:45 +0900

    Add read_FlickrLogos27 method

--- logo_cnn.py
+++ logo_cnn.py
@@ -13,6 +13,21 @@ tf.app.flags.DEFINE_integer("max_steps", 10000, "Number of batches to run.")
 tf.app.flags.DEFINE_integer("image_size", 64, "Size of an input image.")
 
 
+def read_flickrlogos27(train_dir):
+    class FlickrLogos27Record():
+        pass
+
+    result = FlickrLogos27Record()
+
+    label_bytes = 1
+    result.width = 32
+    result.height = 32
+    result.depth = 3
+    image_bytes = result.width * result.height * result.depth
+    record_bytes = label_bytes + image_bytes
+
+
+
 def inference():
     pass
 
@@ -25,6 +40,7 @@ def train():
 def main():
     if not tf.gfile.Exists(FLAGS.train_dir):
         print("Not found: %s" % (FLAGS.train_dir))
+    read_flickrlogos27(FLAGS.train_dir)
     train()
 
 
