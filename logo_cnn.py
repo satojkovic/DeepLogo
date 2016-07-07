030f14d0d5925dbe17e2e2fa09638d9d22282aa4
satojkovic <satojkovic@gmail.com>
2016-07-08 08:14:13 +0900

    Define a logo loss value

--- logo_cnn.py
+++ logo_cnn.py
@@ -12,6 +12,7 @@ tf.app.flags.DEFINE_string(
     "Directory where to write event logs and checkpoint.")
 tf.app.flags.DEFINE_integer("max_steps", 10000, "Number of batches to run.")
 tf.app.flags.DEFINE_integer("image_size", 64, "Size of an input image.")
+tf.app.flags.DEFINE_integer("num_classes", 27, "Number of logo classes.")
 
 
 def read_flickrlogos27(filename_queue):
@@ -53,7 +54,7 @@ def convolutional_layer():
 
     # first layer
     w_conv1 = tf.Variable(tf.truncated_normal([5, 5, 1, 48], stddev=0.1))
-    b_conv1 = tf.Variable(tf.constant([48]))
+    b_conv1 = tf.Variable(tf.constant(0.1, [48]))
     x_expanded = tf.expand_dims(x, 3)
     conv1 = tf.nn.conv2d(x_expanded, w_conv1, strides=(1, 1), padding='SAME')
     h_conv1 = tf.nn.relu(conv1 + b_conv1)
@@ -64,7 +65,7 @@ def convolutional_layer():
 
     # second layer
     w_conv2 = tf.Variable(tf.truncated_normal([5, 5, 48, 64], stddev=0.1))
-    b_conv2 = tf.Variable(tf.constant([64]))
+    b_conv2 = tf.Variable(tf.constant(0.1, [64]))
     conv2 = tf.nn.conv2d(h_pool1, w_conv2, strides=(1, 1), padding='SAME')
     h_conv2 = tf.nn.relu(conv2 + b_conv2)
     h_pool2 = tf.nn.max_pool(h_conv2,
@@ -74,7 +75,7 @@ def convolutional_layer():
 
     # third layer
     w_conv3 = tf.Variable(tf.truncated_normal([5, 5, 64, 128], stddev=0.1))
-    b_conv3 = tf.Variable(constant([128]))
+    b_conv3 = tf.Variable(tf.constant(0.1, [128]))
     conv3 = tf.nn.conv2d(h_pool2, w_conv3, strides=(1, 1), padding='SAME')
     h_conv3 = tf.nn.relu(conv3 + b_conv3)
     h_pool3 = tf.nn.max_pool(h_conv3,
@@ -90,16 +91,30 @@ def inference():
 
     # Densely connected layer
     w_fc1 = tf.Variable(tf.tuncated_normal([32 * 8 * 128, 2048]))
-    b_fc1 = tf.Variable(constant([2048]))
+    b_fc1 = tf.Variable(tf.constant(0.1, [2048]))
     conv_layer_flat = tf.reshape(conv_layer, [-1, 32 * 8 * 128])
     h_fc1 = tf.nn.relu(tf.matmul(conv_layer_flat, w_fc1) + b_fc1)
+
+    # Output layer
+    w_fc2 = tf.Variable(tf.truncated_normal([2048, FLAGS.num_classes]))
+    b_fc2 = tf.Variable(tf.constant(0.1, [FLAGS.num_classes]))
+
+    y = tf.matmul(h_fc1, w_fc2) + b_fc2
+
+    return (x, y, conv_vars + [w_fc1, b_fc1, w_fc2, b_fc2])
 
 
 def train():
     with tf.Graph().as_default():
-        images, labels = cropped_inputs()
+        x, y, params = inference()
 
-        logits = inference()
+        y_ = tf.placeholder(tf.float32, [None, FLAGS.num_classes])
+
+        logo_loss = tf.nn.softmax_cross_entropy_with_logits(
+            tf.reshape(y[:, 1:], [-1, FLAGS.num_classes]),
+            tf.reshape(y_[:, 1:], [-1, FLAGS.num_classes]))
+
+        logo_loss = tf.reduce_sum(logo_loss)
 
 
 def main():
