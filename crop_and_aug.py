98a5c74fdf96b82efb44fa4d48013969421fca40
satojkovic <satojkovic@gmail.com>
2016-07-20 08:12:33 +0900

    Fix main loop

--- crop_and_aug.py
+++ crop_and_aug.py
@@ -4,9 +4,13 @@ import numpy as np
 import os
 from PIL import Image
 from collections import defaultdict
+from itertools import product
 
 CNN_IN_WIDTH = 64
 CNN_IN_HEIGHT = 32
+
+DATA_AUG_POS_SHIFT_MIN = -2
+DATA_AUG_POS_SHIFT_MAX = 2
 
 TRAIN_DIR = 'flickr_logos_27_dataset'
 TRAIN_IMAGE_DIR = os.path.join(TRAIN_DIR, 'flickr_logos_27_dataset_images')
@@ -14,45 +18,108 @@ CROPPED_IMAGE_DIR = os.path.join(TRAIN_DIR,
                                  'flickr_logos_27_dataset_cropped_images')
 
 
+def parse_annot(annot):
+    fn = annot[0].decode('utf-8')
+    class_name = annot[1].decode('utf-8')
+    train_subset_class = annot[2].decode('utf-8')
+    return fn, class_name, train_subset_class
 
-def crop_logos(annot_train):
-    if not os.path.exists(CROPPED_IMAGE_DIR):
-        os.makedirs(CROPPED_IMAGE_DIR)
 
-    # Multiple images are cropped from same file.
-    crop_per_files = defaultdict(int)
+def aug_pos(annot_train, im):
     for annot in annot_train:
-        fn = annot[0].decode('utf-8')
-        class_name = annot[1].decode('utf-8')
-        train_subset_class = annot[2].decode('utf-8')
-
-        crop_per_files[fn] += 1
+        fn, class_name, train_subset_class = parse_annot(annot)
         x1, y1, x2, y2 = list(map(int, annot[3:]))
+        cx = (x2 - x1) // 2
+        cy = (y2 - y1) // 2
+        wid, hgt = (x2 - x1), (y2 - y1)
         if (x2 - x1) <= 0 or (y2 - y1) <= 0:
             print('Skipping:', fn)
             continue
         im = Image.open(os.path.join(TRAIN_IMAGE_DIR, fn))
-        cropped_im = im.crop((x1, y1, x2, y2))
-        resized_im = cropped_im.resize((CNN_IN_WIDTH, CNN_IN_HEIGHT))
+        
+        for sx, sy in product(range(DATA_AUG_POS_SHIFT_MIN, DATA_AUG_POS_SHIFT_MAX), range(DATA_AUG_POS_SHIFT_MIN, DATA_AUG_POS_SHIFT_MAX)):
+            cx = cx + sx
+            cy = cy + sy
+            cropped_im = im.crop((cx - wid//2, cy - hgt//2, cx + wid//2, cy + hgt//2))
+            resized_im = cropped_im.resize(CNN_IN_WIDTH, CNN_IN_HEIGHT)
+            
 
-        _, ext = os.path.splitext(fn)
-        cropped_fn = '_'.join(
-            [fn.split('.')[0], class_name, train_subset_class,
-             str(crop_per_files[fn])]) + ext
-        resized_im.save(os.path.join(CROPPED_IMAGE_DIR, cropped_fn))
+def aug_scale(annot_train, im):
+    pass
 
-        # close image and release its memory.
+
+def aug_rot(annot_train, im):
+    pass
+
+
+def crop_logos(annot, im):
+    x1, y1, x2, y2 = rect_coord(annot[3:])
+    cropped_im = im.crop((x1, y1, x2, y2))
+    cropped_im = cropped_im.resize((CNN_IN_WIDTH, CNN_IN_HEIGHT))
+    return cropped_im
+
+
+def rect_coord(annot_part):
+    return list(map(int, annot_part))
+
+
+def center_wid_hgt(x1, y1, x2, y2):
+    cx = (x2 - x1) // 2
+    cy = (y2 - y1) // 2
+    wid = (x2 - x1)
+    hgt = (y2 - y1)
+    return cx, cy, wid, hgt
+
+
+def is_skip(annot_part):
+    x1, y1, x2, y2 = rect_coord(annot_part)
+    _, _, wid, hgt = center_wid_hgt(x1, y1, x2, y2)
+    if wid <= 0 or hgt <= 0:
+        return True
+    else:
+        return False
+
+
+def crop_and_aug(annot_train):
+    if not os.path.exists(CROPPED_IMAGE_DIR):
+        os.makedirs(CROPPED_IMAGE_DIR)
+
+    for annot in annot_train:
+        # for generating a file name
+        fn, class_name, train_subset_class = parse_annot(annot)
+        
+        # skip if width or height equal zero
+        if is_skip(annot[3:]):
+            print('Skip: ', fn)
+            continue
+
+        # open an image
+        im = Image.open(os.path.join(TRAIN_IMAGE_DIR, fn))
+
+        # normal cropping
+        cropped_im = crop_logos(annot, im)
+
+        # augment by shifting a center
+        aug_pos(annot, im)
+
+        # augment by scaling
+        aug_scale(annot, im)
+        
+        # augment by rotation
+        aug_rot(annot, im)
+
+        # close image file
         im.close()
         cropped_im.close()
-        resized_im.close()
+        
 
-    # check
+    # print results
     org_imgs = [img for img in os.listdir(TRAIN_IMAGE_DIR)]
-    cropped_imgs = [img for img in os.listdir(CROPPED_IMAGE_DIR)]
+    crop_and_aug_imgs = [img for img in os.listdir(CROPPED_IMAGE_DIR)]
     print('original: %d' % (len(org_imgs)))
-    print('cropped: %d' % (len(cropped_imgs)))
+    print('cropped: %d' % (len(crop_and_aug_imgs)))
 
-    return cropped_imgs
+    return crop_and_aug_imgs
 
 
 def main():
@@ -62,10 +129,8 @@ def main():
         dtype='a')
     print('train_annotation: %d, %d ' % (annot_train.shape))
 
-    #
-    # crop logos from an image
-    #
-    cropped_imgs = crop_logos(annot_train)
+    # normal cropping and data augmentation
+    crop_and_aug(annot_train)
 
 if __name__ == '__main__':
     main()
