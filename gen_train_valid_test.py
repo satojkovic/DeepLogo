8122fb6f13eb09f3a9780e8a7c926928cfbf3c57
satojkovic <satojkovic@gmail.com>
2016-07-29 08:09:14 +0900

    Add new script

---
+++ gen_train_valid_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import os
+
+TRAIN_DIR = 'flickr_logos_27_dataset'
+CROPPED_AUG_IMAGE_DIR = os.path.join(TRAIN_DIR,
+                                     'flickr_logos_27_dataset_cropped_augmented_images')
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
