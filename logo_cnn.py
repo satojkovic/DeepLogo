e2e70f89d62cd122f3264a88c82a9462f1dd397a
satojkovic <satojkovic@gmail.com>
2016-08-06 15:35:03 +0900

    Add a model method
    some bug fixes

--- logo_cnn.py
+++ logo_cnn.py
@@ -43,7 +43,7 @@ tf.app.flags.DEFINE_integer("max_steps", 10001, "Number of batches to run.")
 tf.app.flags.DEFINE_integer("image_width", 64, "A width of an input image.")
 tf.app.flags.DEFINE_integer("image_height", 32, "A height of an input image.")
 tf.app.flags.DEFINE_integer("num_classes", 27, "Number of logo classes.")
-tf.app.flags.DEFINE_integer("learning_rate", 0.01, "Learning rate")
+tf.app.flags.DEFINE_integer("learning_rate", 0.001, "Learning rate")
 tf.app.flags.DEFINE_integer("batch_size", 16, "A batch size")
 tf.app.flags.DEFINE_integer("num_channels", 3,
                             "A number of channels of an input image.")
@@ -64,6 +64,55 @@ def reformat(dataset, labels):
     labels = (
         np.arange(FLAGS.num_classes) == labels[:, None]).astype(np.float32)
     return dataset, labels
+
+
+def model(data):
+    # First layer
+    w_conv1 = tf.Variable(
+        tf.truncated_normal(
+            [FLAGS.patch_size, FLAGS.patch_size, FLAGS.num_channels, 48],
+            stddev=0.1))
+    b_conv1 = tf.constant(0.1, shape=[48])
+    h_conv1 = tf.nn.relu(
+        tf.nn.conv2d(
+            data, w_conv1, [1, 1, 1, 1], padding='SAME') + b_conv1)
+    h_pool1 = tf.nn.max_pool(
+        h_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    # Second layer
+    w_conv2 = tf.Variable(
+        tf.truncated_normal(
+            [FLAGS.patch_size, FLAGS.patch_size, 48, 64], stddev=0.1))
+    b_conv2 = tf.constant(0.1, shape=[64])
+    h_conv2 = tf.nn.relu(
+        tf.nn.conv2d(
+            h_pool1, w_conv2, [1, 1, 1, 1], padding='SAME') + b_conv2)
+    h_pool2 = tf.nn.max_pool(
+        h_conv2, ksize=[1, 1, 2, 1], strides=[1, 1, 2, 1], padding='SAME')
+
+    # Third layer
+    w_conv3 = tf.Variable(
+        tf.truncated_normal(
+            [FLAGS.patch_size, FLAGS.patch_size, 64, 128], stddev=0.1))
+    b_conv3 = tf.constant(0.1, shape=[128])
+    h_conv3 = tf.nn.relu(
+        tf.nn.conv2d(
+            h_pool2, w_conv3, [1, 1, 1, 1], padding='SAME') + b_conv3)
+    h_pool3 = tf.nn.max_pool(
+        h_conv3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+
+    # Fully connected layer
+    w_fc1 = tf.Variable(tf.truncated_normal([16 * 4 * 128, 2048], stddev=0.1))
+    b_fc1 = tf.constant(0.1, shape=[2048])
+    conv_layer_flat = tf.reshape(h_pool3, [-1, 16 * 4 * 128])
+    h_fc1 = tf.nn.relu(tf.matmul(conv_layer_flat, w_fc1) + b_fc1)
+
+    # Output layer
+    w_fc2 = tf.Variable(tf.truncated_normal([2048, FLAGS.num_classes]))
+    b_fc2 = tf.constant(0.1, shape=[FLAGS.num_classes])
+    out = tf.matmul(h_fc1, w_fc2) + b_fc2
+
+    return out
 
 
 def main():
@@ -93,60 +142,23 @@ def main():
         # Input data
         tf_train_dataset = tf.placeholder(
             tf.float32,
-            shape=(FLAGS.batch_size, FLAGS.image_width, FLAGS.image_width,
+            shape=(FLAGS.batch_size, FLAGS.image_width, FLAGS.image_height,
                    FLAGS.num_channels))
         tf_train_labels = tf.placeholder(
             tf.float32, shape=(FLAGS.batch_size, FLAGS.num_classes))
         tf_valid_dataset = tf.constant(valid_dataset)
         tf_test_dataset = tf.constant(test_dataset)
 
-        # First layer
-        w_conv1 = tf.Variable(
-            tf.truncated_normal(
-                [FLAGS.patch_size, FLAGS.patch_size, FLAGS.num_channels, 48],
-                stddev=0.1))
-        b_conv1 = tf.constant(0.1, shape=[48])
-        h_conv1 = tf.nn.relu(
-            tf.nn.conv2d(
-                tf_train_dataset, w_conv1, [1, 1, 1, 1], padding='SAME') +
-            b_conv1)
-        h_pool1 = tf.nn.max_pool(
-            h_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+        # Training computation
+        logits = model(tf_train_dataset)
+        loss = tf.reduce_sum(
+            tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
+        optimizer = tf.train.AdamOptimizer(FLAGS.learning_rate).minimize(loss)
 
-        # Second layer
-        w_conv2 = tf.Variable(
-            tf.truncated_normal(
-                [FLAGS.patch_size, FLAGS.patch_size, 48, 64], stddev=0.1))
-        b_conv2 = tf.constant(0.1, shape=[64])
-        h_conv2 = tf.nn.relu(
-            tf.nn.conv2d(
-                h_pool1, w_conv2, [1, 1, 1, 1], padding='SAME') + b_conv2)
-        h_pool2 = tf.nn.max_pool(
-            h_conv2, ksize=[1, 2, 1, 1], strides=[1, 2, 1, 1], padding='SAME')
-
-        # Third layer
-        w_conv3 = tf.Variable(
-            tf.truncated_normal(
-                [FLAGS.patch_size, FLAGS.patch_size, 64, 128], stddev=0.1))
-        b_conv3 = tf.constant(0.1, shape=[128])
-        h_conv3 = tf.nn.relu(
-            tf.nn.conv2d(
-                h_pool2, w_conv3, [1, 1, 1, 1], padding='SAME'))
-        h_pool3 = tf.nn.max_pool(
-            h_conv3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-        # Fully connected layer
-        w_fc1 = tf.Variable(
-            tf.truncated_normal(
-                [16 * 4 * 128, 2048], stddev=0.1))
-        b_fc1 = tf.constant(0.1, shape=[2048])
-        conv_layer_flat = tf.reshape(h_pool3, [-1, 16 * 4 * 128])
-        h_fc1 = tf.nn.relu(tf.matmul(conv_layer_flat, w_fc1) + b_fc1)
-
-        # Output layer
-        w_fc2 = tf.Variable(tf.truncated_normal([2048, FLAGS.num_classes]))
-        b_fc2 = tf.constant(0.1, shape=[FLAGS.num_classes])
-        out = tf.matmul(h_fc1, w_fc2) + b_fc2
+        # Predictions for the training, validation, and test data
+        train_prediction = tf.nn.softmax(logits)
+        valid_prediction = tf.nn.softmax(model(tf_valid_dataset))
+        test_prediction = tf.nn.softmax(model(tf_test_dataset))
 
 
 if __name__ == '__main__':
