9ff2af45e10685844f16edb4e70d77736d6b9789
satojkovic <satojkovic@gmail.com>
2016-08-08 07:27:57 +0900

    Fix model method

--- logo_cnn.py
+++ logo_cnn.py
@@ -38,13 +38,9 @@ def reformat(dataset, labels):
     return dataset, labels
 
 
-def model(data):
+def model(data, w_conv1, b_conv1, w_conv2, b_conv2, w_conv3, b_conv3, w_fc1,
+          b_fc1, w_fc2, b_fc2):
     # First layer
-    w_conv1 = tf.Variable(
-        tf.truncated_normal(
-            [FLAGS.patch_size, FLAGS.patch_size, FLAGS.num_channels, 48],
-            stddev=0.1))
-    b_conv1 = tf.constant(0.1, shape=[48])
     h_conv1 = tf.nn.relu(
         tf.nn.conv2d(
             data, w_conv1, [1, 1, 1, 1], padding='SAME') + b_conv1)
@@ -52,10 +48,6 @@ def model(data):
         h_conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
     # Second layer
-    w_conv2 = tf.Variable(
-        tf.truncated_normal(
-            [FLAGS.patch_size, FLAGS.patch_size, 48, 64], stddev=0.1))
-    b_conv2 = tf.constant(0.1, shape=[64])
     h_conv2 = tf.nn.relu(
         tf.nn.conv2d(
             h_pool1, w_conv2, [1, 1, 1, 1], padding='SAME') + b_conv2)
@@ -63,10 +55,6 @@ def model(data):
         h_conv2, ksize=[1, 1, 2, 1], strides=[1, 1, 2, 1], padding='SAME')
 
     # Third layer
-    w_conv3 = tf.Variable(
-        tf.truncated_normal(
-            [FLAGS.patch_size, FLAGS.patch_size, 64, 128], stddev=0.1))
-    b_conv3 = tf.constant(0.1, shape=[128])
     h_conv3 = tf.nn.relu(
         tf.nn.conv2d(
             h_pool2, w_conv3, [1, 1, 1, 1], padding='SAME') + b_conv3)
@@ -74,14 +62,10 @@ def model(data):
         h_conv3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
     # Fully connected layer
-    w_fc1 = tf.Variable(tf.truncated_normal([16 * 4 * 128, 2048], stddev=0.1))
-    b_fc1 = tf.constant(0.1, shape=[2048])
     conv_layer_flat = tf.reshape(h_pool3, [-1, 16 * 4 * 128])
     h_fc1 = tf.nn.relu(tf.matmul(conv_layer_flat, w_fc1) + b_fc1)
 
     # Output layer
-    w_fc2 = tf.Variable(tf.truncated_normal([2048, FLAGS.num_classes]))
-    b_fc2 = tf.constant(0.1, shape=[FLAGS.num_classes])
     out = tf.matmul(h_fc1, w_fc2) + b_fc2
 
     return out
@@ -111,6 +95,31 @@ def main():
     # Training model
     graph = tf.Graph()
     with graph.as_default():
+        # Variables
+        w_conv1 = tf.Variable(
+            tf.truncated_normal(
+                [FLAGS.patch_size, FLAGS.patch_size, FLAGS.num_channels, 48],
+                stddev=0.1))
+        b_conv1 = tf.constant(0.1, shape=[48])
+
+        w_conv2 = tf.Variable(
+            tf.truncated_normal(
+                [FLAGS.patch_size, FLAGS.patch_size, 48, 64], stddev=0.1))
+        b_conv2 = tf.constant(0.1, shape=[64])
+
+        w_conv3 = tf.Variable(
+            tf.truncated_normal(
+                [FLAGS.patch_size, FLAGS.patch_size, 64, 128], stddev=0.1))
+        b_conv3 = tf.constant(0.1, shape=[128])
+
+        w_fc1 = tf.Variable(
+            tf.truncated_normal(
+                [16 * 4 * 128, 2048], stddev=0.1))
+        b_fc1 = tf.constant(0.1, shape=[2048])
+
+        w_fc2 = tf.Variable(tf.truncated_normal([2048, FLAGS.num_classes]))
+        b_fc2 = tf.constant(0.1, shape=[FLAGS.num_classes])
+
         # Input data
         tf_train_dataset = tf.placeholder(
             tf.float32,
@@ -122,15 +131,21 @@ def main():
         tf_test_dataset = tf.constant(test_dataset)
 
         # Training computation
-        logits = model(tf_train_dataset)
+
+        logits = model(tf_train_dataset, w_conv1, b_conv1, w_conv2, b_conv2,
+                       w_conv3, b_conv3, w_fc1, b_fc1, w_fc2, b_fc2)
         loss = tf.reduce_sum(
             tf.nn.softmax_cross_entropy_with_logits(logits, tf_train_labels))
         optimizer = tf.train.AdamOptimizer(FLAGS.learning_rate).minimize(loss)
 
         # Predictions for the training, validation, and test data
         train_prediction = tf.nn.softmax(logits)
-        valid_prediction = tf.nn.softmax(model(tf_valid_dataset))
-        test_prediction = tf.nn.softmax(model(tf_test_dataset))
+        valid_prediction = tf.nn.softmax(
+            model(tf_valid_dataset, w_conv1, b_conv1, w_conv2, b_conv2,
+                  w_conv3, b_conv3, w_fc1, b_fc1, w_fc2, b_fc2))
+        test_prediction = tf.nn.softmax(
+            model(tf_test_dataset, w_conv1, b_conv1, w_conv2, b_conv2, w_conv3,
+                  b_conv3, w_fc1, b_fc1, w_fc2, b_fc2))
 
     # Do training
     with tf.Session(graph=graph) as session:
