f4a8fb40cf96f5fc42127e70e5a1a9703b0cb858
satojkovic <satojkovic@gmail.com>
2016-08-07 12:28:09 +0900

    Change a default value of learning_rate

--- logo_cnn.py
+++ logo_cnn.py
@@ -43,7 +43,7 @@ tf.app.flags.DEFINE_integer("max_steps", 10001, "Number of batches to run.")
 tf.app.flags.DEFINE_integer("image_width", 64, "A width of an input image.")
 tf.app.flags.DEFINE_integer("image_height", 32, "A height of an input image.")
 tf.app.flags.DEFINE_integer("num_classes", 27, "Number of logo classes.")
-tf.app.flags.DEFINE_integer("learning_rate", 0.001, "Learning rate")
+tf.app.flags.DEFINE_integer("learning_rate", 0.0001, "Learning rate")
 tf.app.flags.DEFINE_integer("batch_size", 16, "A batch size")
 tf.app.flags.DEFINE_integer("num_channels", 3,
                             "A number of channels of an input image.")
