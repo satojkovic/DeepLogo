18f32190e66e3b26b326573a2b5073fc30875eb6
satojkovic <satojkovic@gmail.com>
2016-08-13 13:14:49 +0900

    Fix indent

--- crop_and_aug.py
+++ crop_and_aug.py
@@ -30,7 +30,6 @@ from itertools import product
 from sklearn.cross_validation import train_test_split
 import shutil
 
-
 CNN_IN_WIDTH = 64
 CNN_IN_HEIGHT = 32
 
@@ -42,8 +41,8 @@ DATA_AUG_ROT_MAX = 15
 
 TRAIN_DIR = 'flickr_logos_27_dataset'
 TRAIN_IMAGE_DIR = os.path.join(TRAIN_DIR, 'flickr_logos_27_dataset_images')
-CROPPED_AUG_IMAGE_DIR = os.path.join(TRAIN_DIR,
-                                     'flickr_logos_27_dataset_cropped_augmented_images')
+CROPPED_AUG_IMAGE_DIR = os.path.join(
+    TRAIN_DIR, 'flickr_logos_27_dataset_cropped_augmented_images')
 ANNOT_FILE = 'flickr_logos_27_dataset_training_set_annotation.txt'
 
 
@@ -52,6 +51,7 @@ def parse_annot(annot):
     class_name = annot[1].decode('utf-8')
     train_subset_class = annot[2].decode('utf-8')
     return fn, class_name, train_subset_class
+
 
 def get_rect(annot):
     rect = defaultdict(int)
@@ -73,14 +73,16 @@ def aug_pos(annot, im):
     aug_pos_suffixes = []
 
     rect = get_rect(annot)
-    for sx, sy in product(range(DATA_AUG_POS_SHIFT_MIN, DATA_AUG_POS_SHIFT_MAX), range(DATA_AUG_POS_SHIFT_MIN, DATA_AUG_POS_SHIFT_MAX)):
+    for sx, sy in product(
+            range(DATA_AUG_POS_SHIFT_MIN, DATA_AUG_POS_SHIFT_MAX),
+            range(DATA_AUG_POS_SHIFT_MIN, DATA_AUG_POS_SHIFT_MAX)):
         cx = rect['cx'] + sx
         cy = rect['cy'] + sy
         cropped_im = im.crop((cx - rect['wid'] // 2, cy - rect['hgt'] // 2,
                               cx + rect['wid'] // 2, cy + rect['hgt'] // 2))
         resized_im = cropped_im.resize((CNN_IN_WIDTH, CNN_IN_HEIGHT))
         aug_pos_ims.append(resized_im)
-        aug_pos_suffixes.append('p' +  str(sx) + str(sy))
+        aug_pos_suffixes.append('p' + str(sx) + str(sy))
         cropped_im.close()
 
     return aug_pos_ims, aug_pos_suffixes
@@ -111,8 +113,9 @@ def aug_rot(annot, im):
     rect = get_rect(annot)
     for r in range(DATA_AUG_ROT_MIN, DATA_AUG_ROT_MAX):
         rotated_im = im.rotate(r)
-        cropped_im = rotated_im.crop((rect['cx'] - rect['wid'] // 2, rect['cy'] - rect['hgt'] // 2,
-                                      rect['cx'] + rect['wid'] // 2, rect['cy'] + rect['hgt'] // 2))
+        cropped_im = rotated_im.crop(
+            (rect['cx'] - rect['wid'] // 2, rect['cy'] - rect['hgt'] // 2,
+             rect['cx'] + rect['wid'] // 2, rect['cy'] + rect['hgt'] // 2))
         resized_im = cropped_im.resize((CNN_IN_WIDTH, CNN_IN_HEIGHT))
         aug_rot_ims.append(resized_im)
         aug_rot_suffixes.append('r' + str(r))
@@ -158,8 +161,9 @@ def save_im(annot, cnt, *args):
         os.makedirs(dst_dir)
     for i, arg in enumerate(args):
         for im, suffix in zip(arg[0], arg[1]):
-            save_fn = '_'.join([fn.split('.')[0], class_name,
-                                train_subset_class, str(cnt), suffix]) + os.path.splitext(fn)[1]
+            save_fn = '_'.join(
+                [fn.split('.')[0], class_name, train_subset_class, str(cnt),
+                 suffix]) + os.path.splitext(fn)[1]
             im.save(os.path.join(dst_dir, save_fn))
 
 
@@ -178,7 +182,7 @@ def crop_and_aug(annot_train):
         # for generating a file name
         fn, _, _ = parse_annot(annot)
         cnt_per_file[fn] += 1
-        
+
         # skip if width or height equal zero
         if is_skip(annot[3:]):
             print('Skip: ', fn)
@@ -195,15 +199,13 @@ def crop_and_aug(annot_train):
 
         # augment by scaling
         scaled_ims, scaled_suffixes = aug_scale(annot, im)
-        
+
         # augment by rotation
         rotated_ims, rotated_suffixes = aug_rot(annot, im)
 
         # save images
-        save_im(annot, cnt_per_file[fn],
-                [cropped_ims, cropped_suffixes],
-                [shifted_ims, shifted_suffixes],
-                [scaled_ims, scaled_suffixes],
+        save_im(annot, cnt_per_file[fn], [cropped_ims, cropped_suffixes],
+                [shifted_ims, shifted_suffixes], [scaled_ims, scaled_suffixes],
                 [rotated_ims, rotated_suffixes])
 
         # close image file
@@ -221,8 +223,9 @@ def crop_and_aug(annot_train):
 def do_train_test_split():
     class_names = [cls for cls in os.listdir(CROPPED_AUG_IMAGE_DIR)]
     for class_name in class_names:
-        imgs = [img for img in os.listdir(os.path.join(CROPPED_AUG_IMAGE_DIR,
-                                                       class_name))]
+        imgs = [img
+                for img in os.listdir(
+                    os.path.join(CROPPED_AUG_IMAGE_DIR, class_name))]
         # train=0.75, test=0.25
         train_imgs, test_imgs = train_test_split(imgs)
         # move to train or test directory
