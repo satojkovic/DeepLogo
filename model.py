316964189433f7cb11169bb74a5e5d2ab91c1d8c
satojkovic <satojkovic@gmail.com>
2018-01-02 00:43:07 +0900

    Use xavier initializer

--- model.py
+++ model.py
@@ -34,24 +34,34 @@ def params():
     # weights and biases
     params = {}
 
-    params['w_conv1'] = tf.Variable(
-        tf.truncated_normal(
-            [PATCH_SIZE, PATCH_SIZE, common.CNN_IN_CH, 32], stddev=0.1))
+    params['w_conv1'] = tf.get_variable(
+        'w_conv1',
+        shape=[PATCH_SIZE, PATCH_SIZE, common.CNN_IN_CH, 32],
+        initializer=tf.contrib.layers.xavier_initializer())
     params['b_conv1'] = tf.Variable(tf.constant(0.1, shape=[32]))
 
-    params['w_conv2'] = tf.Variable(
-        tf.truncated_normal([PATCH_SIZE, PATCH_SIZE, 32, 64], stddev=0.1))
+    params['w_conv2'] = tf.get_variable(
+        'w_conv2',
+        shape=[PATCH_SIZE, PATCH_SIZE, 32, 64],
+        initializer=tf.contrib.layers.xavier_initializer())
     params['b_conv2'] = tf.Variable(tf.constant(0.1, shape=[64]))
 
-    params['w_conv3'] = tf.Variable(
-        tf.truncated_normal([PATCH_SIZE, PATCH_SIZE, 64, 128], stddev=0.1))
+    params['w_conv3'] = tf.get_variable(
+        'w_conv3',
+        shape=[PATCH_SIZE, PATCH_SIZE, 64, 128],
+        initializer=tf.contrib.layers.xavier_initializer())
     params['b_conv3'] = tf.Variable(tf.constant(0.1, shape=[128]))
 
-    params['w_fc1'] = tf.Variable(
-        tf.truncated_normal([16 * 4 * 128, 2048], stddev=0.1))
+    params['w_fc1'] = tf.get_variable(
+        'w_fc1',
+        shape=[16 * 4 * 128, 2048],
+        initializer=tf.contrib.layers.xavier_initializer())
     params['b_fc1'] = tf.Variable(tf.constant(0.1, shape=[2048]))
 
-    params['w_fc2'] = tf.Variable(tf.truncated_normal([2048, NUM_CLASSES]))
+    params['w_fc2'] = tf.get_variable(
+        'w_fc2',
+        shape=[2048, NUM_CLASSES],
+        initializer=tf.contrib.layers.xavier_initializer())
     params['b_fc2'] = tf.Variable(tf.constant(0.1, shape=[NUM_CLASSES]))
 
     return params
